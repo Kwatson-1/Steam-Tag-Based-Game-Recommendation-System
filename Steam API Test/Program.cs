566dd527320d2f35c7f806c664efa5d7d0bdf47f
Kyle Watson <kyle.p.watson@gmail.com>
2023-04-12 22:42:17 +0800

    Testing Steam API
    Using API key to test the JSON response for a single game.

---
+++ Steam API Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace SteamGameInfoRetriever
+{
+    class Program
+    {
+        private static readonly string appId = "435150"; // Divinity 2: Original Sin AppID
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string gameInfoJson = await GetGameInfoAsync(appId);
+                Console.WriteLine("Game information for Divinity 2: Original Sin:");
+                Console.WriteLine(gameInfoJson);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private static async Task<string> GetGameInfoAsync(string appId)
+        {
+            string requestUrl = $"https://store.steampowered.com/api/appdetails/?appids={appId}";
+            HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                JObject jsonResponse = JObject.Parse(content);
+                JObject gameInfo = (JObject)jsonResponse[appId]["data"];
+                return gameInfo.ToString();
+            }
+            else
+            {
+                throw new Exception("Failed to retrieve game information.");
+            }
+        }
+    }
+}
