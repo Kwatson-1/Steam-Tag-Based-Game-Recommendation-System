5ad4bbd3a0570966b608c6e123c4fb72a89012c9
Kyle Watson <kyle.p.watson@gmail.com>
2023-04-18 23:08:02 +0800

    Database diagram & console application goals added

--- SteamAppDetailsToSQL/Program.cs
+++ SteamAppDetailsToSQL/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+Developer: Kyle Watson
+Date: 18/04/2023
+Project Tasks/Goals
+    1. Program will fetch a list of all Steam apps which includes their name and ID from the Steam API using the GetSteamGamesAsync endpoint. 
+    2. Stores the information in a CSV file for backup purposes and to minimize api calls.
+    3. AppIDs are then pulled from the CSV file and stored in a List.
+    4. The List is then looped through to pull the AppID which is then used in a call to the Steam API using the GetGameDetailsAsync endpoint.
+    4a. Use Task.Delay to introduce a delay between each call to avoid potential issues or bans.
+    5. Game information is stored in a JSON file for backup purposes and to minimize api calls.
+    6. Call back the data fro the JSON file and parse the JSON response for each app and extract the necessary data.
+    7. Use a library to connect the SQL Server dataase and insert the extracted data.
+*/
 namespace SteamAppDetailsToSQL
 {
     internal class Program
@@ -11,5 +24,6 @@ namespace SteamAppDetailsToSQL
         static void Main(string[] args)
         {
         }
+
     }
 }
