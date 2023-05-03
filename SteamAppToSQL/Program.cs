05a7ac713c305293e974d5e4ae52b4b0756a8071
Kyle Watson <kyle.p.watson@gmail.com>
2023-05-03 23:18:39 +0800

    Misc. update
    Changes to classes.
    Handling of null variables.
    Removal of connection string from source code to external JSON file.
    Port from Console Application (.NET framework) to Console Application (.NET Core)

--- SteamAppToSQL/Program.cs
+++ SteamAppToSQL/Program.cs
@@ -17,7 +17,9 @@ using System.Formats.Asn1;
 using System.Net.Http.Json;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
 
 /* 
 Developer: Kyle Watson
@@ -26,7 +28,7 @@ Project Tasks/Goals
     /1. Program will fetch a list of all Steam apps which includes their name and ID from the Steam API using the GetSteamGamesAsync endpoint. 
     /2. Stores the information in a CSV file for backup purposes and to minimize api calls.
     /3. AppIDs are then pulled from the CSV file and stored in a List.
-    4. The List is then looped through to pull the AppID which is then used in a call to the Steam API using the GetGameDetailsAsync endpoint.
+    4. The List is then looped through to pull the AppID which is then used in a call to the Steam API using the GetGameDetailsAsync & GetAppReviewsAsync endpoint.
     4a. Use Task.Delay to introduce a delay between each call to avoid potential issues or bans.
     5. Game information is stored in a JSON file for backup purposes and to minimize api calls.
     6. Call back the data fro the JSON file and parse the JSON response for each app and extract the necessary data.
@@ -45,12 +47,14 @@ namespace SteamAppDetailsToSQL
             //Console.WriteLine($"Saved {games.Count} games to {outputPath}");
 
             //List<SteamApp> appList = GetSteamAppsFromCsv();
+
             int appId = 294100; // Replace with the desired app ID
             var details = await FetchAppDetailsAsync(appId);
             var reviews = await FetchAppReviewsAsync(appId);
             MergedData md = new(details, reviews);
-            CreateDbContext(md);
             Console.ReadLine();
+
+
         }
         #region App List Methods
         // Returns the App List from the Steam API endpoint.
@@ -58,12 +62,12 @@ namespace SteamAppDetailsToSQL
         {
             using var httpClient = new HttpClient();
             string url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
-            string response = await httpClient.GetStringAsync(url);
 
+            string response = await httpClient.GetStringAsync(url) ?? throw new Exception("Failed to retrieve data from the API.");
             JObject jsonResponse = JObject.Parse(response);
-            JArray appsArray = (JArray)jsonResponse["applist"]["apps"];
 
-            List<SteamApp> appList = appsArray.ToObject<List<SteamApp>>();
+            JArray appsArray = (JArray)(jsonResponse["applist"]?["apps"] ?? throw new Exception("Failed to locate data within the JSON response."));
+            List<SteamApp> appList = appsArray.ToObject<List<SteamApp>>() ?? throw new Exception("There was no object to store within the List.");
             return appList;
         }
         // Writes the Steam apps list to a CSV file.
@@ -78,9 +82,9 @@ namespace SteamAppDetailsToSQL
         public static List<SteamApp> GetSteamAppsFromCsv()
         {
             List<SteamApp> steamApps = new();
-            using (StreamReader sr = new ("steam_games.csv"))
+            using (StreamReader sr = new("steam_games.csv"))
             {
-                string line;
+                string? line;
                 while ((line = sr.ReadLine()) != null)
                 {
                     string[] values = line.Split(',');
@@ -116,7 +120,7 @@ namespace SteamAppDetailsToSQL
 
             // Deserialize the JSON response
 
-            DetailsRoot myDeserializedDetailsClass = JsonConvert.DeserializeObject<DetailsRoot>(jsonDetailsResponseInner);
+            DetailsRoot myDeserializedDetailsClass = JsonConvert.DeserializeObject<DetailsRoot>(jsonDetailsResponseInner) ?? throw new Exception("There was no data to deserialize.");
 
             return myDeserializedDetailsClass;
         }
@@ -133,48 +137,49 @@ namespace SteamAppDetailsToSQL
 
             // Deserialize the JSON response
 
-            ReviewRoot myDeserializedReviewClass = JsonConvert.DeserializeObject<ReviewRoot>(jsonReviewResponse);
+            ReviewRoot myDeserializedReviewClass = JsonConvert.DeserializeObject<ReviewRoot>(jsonReviewResponse) ?? throw new Exception("There was no data to deserialize.");
 
             return myDeserializedReviewClass;
         }
         public static string ExtractInnerJson(string jsonString)
         {
             JObject outerObject = JObject.Parse(jsonString);
-            JProperty firstProperty = outerObject.Properties().FirstOrDefault();
+            JProperty? firstProperty = outerObject.Properties().FirstOrDefault();
 
             // If the inner object is null, return
             if (firstProperty == null)
             {
-                return null;
+                return string.Empty;
             }
             JObject innerObject = (JObject)firstProperty.Value;
             return innerObject.ToString();
 
         }
         #endregion
-        public static void CreateDbContext(MergedData md)
+
+        public static string GetConnectionString(string connectionName)
         {
-            // New instance of the DbContext class
-            using var db = new SteamDbContext();
-            // Add a new product to the database
-            db.MergedDataObject.AddRange(md);
-            db.SaveChanges();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            return configuration.GetConnectionString(connectionName) ?? throw new Exception("Unable to resolve connection string");
         }
     }
 
-    // Create a DbContext class that represents your database
+    // Creates a DbContext class that represents the database
     public class SteamDbContext : DbContext
     {
-        public DbSet<MergedData> MergedDataObject { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Set the connection string for your database
-            optionsBuilder.UseSqlServer("Server=KYLES-LAPTOP;Database=SteamAppDatabase;Trusted_Connection=True;");
+            string connectionString = Program.GetConnectionString("SteamAppDatabase");
+            optionsBuilder.UseSqlServer(connectionString);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<MergedData>().HasNoKey();
+
         }
     }
 
@@ -201,9 +206,9 @@ namespace SteamAppDetailsToSQL
     [NotMapped]
     public class MergedData
     {
-        public DetailsRoot DetailsObject { get; set; }
+        public DetailsRoot? DetailsObject { get; set; }
 
-        public ReviewRoot ReviewObject { get; set; }
+        public ReviewRoot? ReviewObject { get; set; }
 
         public MergedData() { }
         public MergedData(DetailsRoot dR, ReviewRoot rR)
@@ -216,25 +221,18 @@ namespace SteamAppDetailsToSQL
     #endregion
     #region Review Classes
     // Classes for deserializing results from the 
-    [NotMapped]
     public class ReviewRoot
     {
         [JsonProperty("success")]
         public int Success { get; set; }
 
         [JsonProperty("query_summary")]
-        public QuerySummary QuerySummary { get; set; }
+        public QuerySummary? QuerySummary { get; set; }
     }
     public class QuerySummary
     {
-        [JsonProperty("num_reviews")]
-        public int NumReviews { get; set; }
-
-        [JsonProperty("review_score")]
-        public int ReviewScore { get; set; }
-
         [JsonProperty("review_score_desc")]
-        public string ReviewScoreDesc { get; set; }
+        public string ReviewScoreDesc { get; set; } = "No user reivew";
 
         [JsonProperty("total_positive")]
         public int TotalPositive { get; set; }
@@ -248,117 +246,125 @@ namespace SteamAppDetailsToSQL
     #endregion
     #region Details Classes
     // Details Classes
-    [NotMapped]
     public class DetailsRoot
     {
         [JsonProperty("success")]
         public bool Success { get; set; }
 
         [JsonProperty("data")]
-        public Data Data { get; set; }
+        public Data? Data { get; set; }
     }
 
     public class Data
     {
-        [Key]
-        public int Id { get; set; }
-        [JsonProperty("type")]
-        public string Type { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
         [JsonProperty("steam_appid")]
         public int SteamAppid { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; } = "Not given";
+
+        [JsonProperty("name")]
+        public string Name { get; set; } = "Not given";
 
         [JsonProperty("required_age")]
         public int RequiredAge { get; set; }
 
         [JsonProperty("is_free")]
-        public bool IsFree { get; set; }
+        public bool IsFree { get; set; } = false;
 
         [JsonProperty("detailed_description")]
-        public string DetailedDescription { get; set; }
+        public string? DetailedDescription { get; set; }
 
         [JsonProperty("about_the_game")]
-        public string AboutTheGame { get; set; }
+        public string? AboutTheGame { get; set; }
 
         [JsonProperty("short_description")]
-        public string ShortDescription { get; set; }
+        public string? ShortDescription { get; set; }
 
         [JsonProperty("supported_languages")]
-        public string SupportedLanguages { get; set; }
+        public string? SupportedLanguages { get; set; }
 
         [JsonProperty("header_image")]
-        public string HeaderImage { get; set; }
+        public string? HeaderImage { get; set; }
 
         [JsonProperty("website")]
-        public string Website { get; set; }
+        public string? Website { get; set; }
 
         [JsonProperty("pc_requirements")]
-        public PcRequirements PcRequirements { get; set; }
+        public PcRequirements? PcRequirements { get; set; }
 
         [JsonProperty("mac_requirements")]
-        public MacRequirements MacRequirements { get; set; }
+        public MacRequirements? MacRequirements { get; set; }
 
         [JsonProperty("linux_requirements")]
-        public LinuxRequirements LinuxRequirements { get; set; }
+        public LinuxRequirements? LinuxRequirements { get; set; }
 
         [JsonProperty("developers")]
-        public List<string> Developers { get; set; }
+        public List<string>? Developers { get; set; }
 
         [JsonProperty("publishers")]
-        public List<string> Publishers { get; set; }
+        public List<string>? Publishers { get; set; }
 
         [JsonProperty("price_overview")]
-        public PriceOverview PriceOverview { get; set; }
+        public PriceOverview? PriceOverview { get; set; }
 
         [JsonProperty("platforms")]
-        public Platforms Platforms { get; set; }
+        public Platforms? Platforms { get; set; }
 
         [JsonProperty("metacritic")]
-        public Metacritic Metacritic { get; set; }
+        public Metacritic? Metacritic { get; set; }
 
         [JsonProperty("screenshots")]
-        public List<Screenshot> Screenshots { get; set; }
+        public List<Screenshot>? Screenshots { get; set; }
 
         [JsonProperty("movies")]
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
 
         [JsonProperty("recommendations")]
-        public Recommendations Recommendations { get; set; }
+        public Recommendations? Recommendations { get; set; }
 
         [JsonProperty("release_date")]
-        public ReleaseDate ReleaseDate { get; set; }
+        public ReleaseDate? ReleaseDate { get; set; }
 
         [JsonProperty("support_info")]
-        public SupportInfo SupportInfo { get; set; }
+        public SupportInfo? SupportInfo { get; set; }
 
         [JsonProperty("background")]
-        public string Background { get; set; }
+        public string? Background { get; set; }
 
         [JsonProperty("background_raw")]
-        public string BackgroundRaw { get; set; }
+        public string? BackgroundRaw { get; set; }
     }
-
+    public class PcRequirements
+    {
+        [JsonProperty("minimum")]
+        public string? Minimum { get; set; }
+    }
     public class LinuxRequirements
     {
         [JsonProperty("minimum")]
-        public string Minimum { get; set; }
+        public string? Minimum { get; set; }
     }
-
+    //public class Developer
+    //{
+    //    public string? name { get; set;}
+    //}
+    //public class Publisher
+    //{
+    //    public string? name { get; set; }
+    //}
     public class MacRequirements
     {
         [JsonProperty("minimum")]
-        public string Minimum { get; set; }
+        public string? Minimum { get; set; }
     }
 
     public class Metacritic
     {
         [JsonProperty("score")]
-        public int Score { get; set; }
+        public int? Score { get; set; }
 
         [JsonProperty("url")]
-        public string Url { get; set; }
+        public string? Url { get; set; }
     }
 
     public class Movie
@@ -367,16 +373,16 @@ namespace SteamAppDetailsToSQL
         public int Id { get; set; }
 
         [JsonProperty("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [JsonProperty("thumbnail")]
-        public string Thumbnail { get; set; }
+        public string? Thumbnail { get; set; }
 
         [JsonProperty("webm")]
-        public Webm Webm { get; set; }
+        public Webm? Webm { get; set; }
 
         [JsonProperty("mp4")]
-        public Mp4 Mp4 { get; set; }
+        public Mp4? Mp4 { get; set; }
 
         [JsonProperty("highlight")]
         public bool Highlight { get; set; }
@@ -385,34 +391,28 @@ namespace SteamAppDetailsToSQL
     public class Mp4
     {
         [JsonProperty("480")]
-        public string _480 { get; set; }
+        public string? _480 { get; set; }
 
         [JsonProperty("max")]
-        public string Max { get; set; }
-    }
-
-    public class PcRequirements
-    {
-        [JsonProperty("minimum")]
-        public string Minimum { get; set; }
+        public string? Max { get; set; }
     }
 
     public class Platforms
     {
         [JsonProperty("windows")]
-        public bool Windows { get; set; }
+        public bool Windows { get; set; } = false;
 
         [JsonProperty("mac")]
-        public bool Mac { get; set; }
+        public bool Mac { get; set; } = false;
 
         [JsonProperty("linux")]
-        public bool Linux { get; set; }
+        public bool Linux { get; set; } = false;
     }
 
     public class PriceOverview
     {
         [JsonProperty("currency")]
-        public string Currency { get; set; }
+        public string? Currency { get; set; }
 
         [JsonProperty("initial")]
         public int Initial { get; set; }
@@ -424,10 +424,10 @@ namespace SteamAppDetailsToSQL
         public int DiscountPercent { get; set; }
 
         [JsonProperty("initial_formatted")]
-        public string InitialFormatted { get; set; }
+        public string? InitialFormatted { get; set; }
 
         [JsonProperty("final_formatted")]
-        public string FinalFormatted { get; set; }
+        public string? FinalFormatted { get; set; }
     }
 
     public class Recommendations
@@ -442,7 +442,7 @@ namespace SteamAppDetailsToSQL
         public bool ComingSoon { get; set; }
 
         [JsonProperty("date")]
-        public string Date { get; set; }
+        public string? Date { get; set; }
     }
 
     public class Screenshot
@@ -451,28 +451,28 @@ namespace SteamAppDetailsToSQL
         public int Id { get; set; }
 
         [JsonProperty("path_thumbnail")]
-        public string PathThumbnail { get; set; }
+        public string? PathThumbnail { get; set; }
 
         [JsonProperty("path_full")]
-        public string PathFull { get; set; }
+        public string? PathFull { get; set; }
     }
 
     public class SupportInfo
     {
         [JsonProperty("url")]
-        public string Url { get; set; }
+        public string? Url { get; set; }
 
         [JsonProperty("email")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
     }
 
     public class Webm
     {
         [JsonProperty("480")]
-        public string _480 { get; set; }
+        public string? _480 { get; set; }
 
         [JsonProperty("max")]
-        public string Max { get; set; }
+        public string? Max { get; set; }
     }
     #endregion
 }
